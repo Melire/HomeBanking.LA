55cdfe2c096cccabf33f682938aadeabbea2a614
Melire <melisarenovales@gmail.com>
2023-10-27 20:49:40 -0300

    creacion del proyecto

---
+++ src/test/java/com/lamlvbank/homebanking/HomeBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamlvbank.homebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
