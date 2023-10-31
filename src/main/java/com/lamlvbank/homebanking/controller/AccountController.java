d1787f105edd2d99e59921b6677a3cd5a7eca65d
FranHasenauer <franciscohasenauer@gmail.com>
2023-10-31 16:04:37 -0300

    Validation
    Creacion de ValidExceptionHandler

--- src/main/java/com/lamlvbank/homebanking/controller/AccountController.java
+++ src/main/java/com/lamlvbank/homebanking/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.lamlvbank.homebanking.controller;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,7 +34,7 @@ public class AccountController {
     }
 
     @PostMapping
-    ResponseEntity<Account> save(@RequestBody Account account) {
+    ResponseEntity<Account> save(@Valid  @RequestBody Account account) {
 
         Account accSaved = accountServ.save(account);
         if (accSaved != null) {
--- src/main/java/com/lamlvbank/homebanking/model/Account.java
+++ src/main/java/com/lamlvbank/homebanking/model/Account.java
@@ -1,10 +1,8 @@
 package com.lamlvbank.homebanking.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,10 +17,26 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idA;
-    private Long accountN;
-    private Long cbu;
-    private String alias;
-    private float amount;
 
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 11)
+    @Column(unique = true, nullable = false)
+    private String accountN;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 22)
+    @Column(unique = true, nullable = false)
+    private String cbu;
+
+
+    @NotNull @NotBlank
+    //@Pattern(regexp = "^[a-z]+\\\\.[a-z]+\\\\.[a-z]+$")//asigna el patron para el alias
+    @Column(unique = true,nullable = false)
+    private String alias;
+
+    @NotNull @DecimalMin(value="0.0") @DecimalMax(value="20000000.0")
+    private float balance;
 
 }
