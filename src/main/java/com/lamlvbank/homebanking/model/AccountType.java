01bb0f7b77feb7787467c31de0d2c59d2b3b8871
Melire <melisarenovales@gmail.com>
2023-11-03 21:21:06 -0300

    Agregado @Validation Pattern

--- src/main/java/com/lamlvbank/homebanking/model/AccountType.java
+++ src/main/java/com/lamlvbank/homebanking/model/AccountType.java
@@ -3,6 +3,7 @@ package com.lamlvbank.homebanking.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
@@ -20,6 +21,7 @@ public class AccountType {
 
     @NotNull  @NotBlank @Size(min=8, max=40)
     @Column(unique = true,nullable=false)
+    @Pattern(regexp ="^Account\\s([A-Za-z]+)$",message="Patron erroneo, debe ser Account [Palabra]")
     //@Length(min=8, max=40) combina @Notblank y @Size
     private String name;
 
