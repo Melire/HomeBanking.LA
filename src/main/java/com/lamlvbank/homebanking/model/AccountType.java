d55475788ad487336d8cb62a0c1da0ed74ad2c6e
Melire <melisarenovales@gmail.com>
2023-11-24 20:40:14 -0300

    refactorizado de mdelo 'creation Date agregado

--- src/main/java/com/lamlvbank/homebanking/model/AccountType.java
+++ src/main/java/com/lamlvbank/homebanking/model/AccountType.java
@@ -1,11 +1,13 @@
 package com.lamlvbank.homebanking.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
@@ -25,6 +27,9 @@ public class AccountType {
     @Pattern(regexp = "^Account\\s([A-Za-z]+)$", message = "Patron erroneo, debe ser Account [Palabra]")
     //@Length(min=8, max=40) combina @Notblank y @Size
     private String name;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",shape = JsonFormat.Shape.STRING)
+    private LocalDateTime creationDate;
 
     @OneToMany(mappedBy = "accountType")
     private List<Account> account;
